b18ebf03bd952d3e8d7c8c3ae0cbce0318c827c1
kraj2503 <srmkr7800@gmail.com>
2024-03-08 03:28:40 +0530

    Basic Todo APP

--- react/first-project/src/App.jsx
+++ react/first-project/src/App.jsx
@@ -1,18 +1,66 @@
-import { useState } from 'react'
-import './App.css'
+// import { useState } from "react";
+
+
+// function App() { //component
+
+//   const [count, setCount] = useState(0);
+
+
+//   return (
+
+
+//     <div>
+//       <CustomButton count={count} setCount={setCount}></CustomButton>
+//     </div>
+
+
+//   )
+// }
+
+// function CustomButton(props) {
+//   function onClickhandler() {
+//     props.setCount(props.count + 1);
+//   }
+//   return <button onClick={onClickhandler}>
+//       Counter {props.count}
+//     </button>
+// }
+
+// export default App
+
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(567)
+  const [todos, setTodos] = useState([{
+    title: "Study",
+    description: "Learn react from 12 to 4am"
+
+  }]);
 
   return (
-    
-     
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div>
       
+    {todos.map(function (todo) {
+      return <Todo title={todo.title} description={todo.description} />
+    })}
+    <button onClick={()=>{
+        setTodos([...todos,{
+          title:"Title",
+          description:"Description"
+        }])
+      }}>Add items</button>
     
+    </div>
   )
+
+}
+
+function Todo(props) {
+
+  return <div>
+    <h1>{props.title}</h1>
+    <h2>{props.description}</h2>
+  </div>
 }
 
 export default App
