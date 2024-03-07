284d7bb693f16e5d9eba1066a2001fe5bddcdcc9
kraj2503 <srmkr7800@gmail.com>
2024-03-07 23:41:11 +0530

    mongo revised and react started

---
+++ react/first-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(567)
+
+  return (
+    
+     
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      
+    
+  )
+}
+
+export default App
